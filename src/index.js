b5daf5f15340b186fd29a0031b1908d1f163d05e
zktnguyen <zktnguyen@ucdavis.edu>
2017-11-16 17:14:16 -0800

    forgot process.env.PORT

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => {
   res.json({ message: "Hey welcome to Node.js & Express" });
 });
 
-app.listen(3000, () => console.log("Listening to port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Listening to port 3000"));
